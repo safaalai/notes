80e303e13a9bde8305e4b21f2c2b8e12bcd543e3
Safa Courses <safacourses@Safas-16-MacBook.local>
2020-08-06 18:53:53 -0700

    fixing CORS access

--- server/gateway/src/app.ts
+++ server/gateway/src/app.ts
@@ -1,6 +1,10 @@
 /** Main application router for gateway */
 import express from 'express';
+import cors from 'cors';
 export const app = express();
+
+app.use(cors());
+app.options('*', cors());
 
 app.get('/', (req,res) => {
   console.log('get /');
