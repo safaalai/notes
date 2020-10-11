cd36adc4c1b175ac9a41d5a47e62c96d71b2decd
Safa Courses <safacourses@Safas-16-MacBook.local>
2020-10-10 20:59:40 -0700

    json2db with CLI & config

--- database/src/json2db.ts
+++ database/src/json2db.ts
@@ -1,16 +1,38 @@
 import axios from 'axios';
 import fs from 'fs';
-import { defaultArgs } from 'puppeteer';
+import argv from 'minimist';
 
-/** json2db takes a script file and operates on HarperDB */
+/** json2db takes a script file and operates on HarperDB 
+ * Command line arguments:
+ * --target=<target environment>
+ * --script=<script to run>
+ * 
+ * Must have specified dbConfig.json
+*/
+
+// Get & check command line arguments
+const args = argv(process.argv.slice(2));
+if(!('target' in args) || !('script' in args)) {
+  console.log('usage: json2db --target=<target> --script=<dbScript>');
+  console.log('  target= db target in config file');
+  console.log('  script= JSON script file in scripts folder');
+  process.exit(0);
+}
+console.log(args);
+
+// Get dbConfig
+const configPath = './dbconfig.json';
+const config = JSON.parse(fs.readFileSync(configPath).toString());
+const targetConfig = config[args.target];
+console.log(targetConfig);
 
 // Set up DB parameters
-const username = 'HDB_ADMIN';
-const password = 'password';
-const url = 'http://localhost:9925';
+const username = targetConfig.username;
+const password = targetConfig.password;
+const url = targetConfig.url;
 
 // Set up database script
-const dbScriptPath = './scripts/' + 'sys-info' + '.json';
+const dbScriptPath = './scripts/' + args.script + '.json';
 const dbScript = JSON.parse(fs.readFileSync(dbScriptPath).toString());
 
 // Set up http configuration
